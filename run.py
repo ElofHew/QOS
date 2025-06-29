18d0e34aee28805d0b29cefe7090c0e93f95825b
ElofHew <2352235176@qq.com>
2025-06-29 15:29:07 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,46 +2,77 @@
 # Quarter Operation System (QOS)
 # 1 Step to run
 @ Author: ElofHew aka Dan_Evan
-@ Date: 2025-06-22
+@ Date: 2025-06-29
+@ Version: 0.2
 """
 
 import os
 import sys
 import platform
 
+try:
+    if not os.path.exists(os.path.join(os.getcwd(),"QOS","qos.py")) and os.path.exists(os.path.join(os.getcwd(),"QOS","system")):
+        raise FileNotFoundError
+    if not sys.version_info >= (3, 10):
+        raise ValueError
+except FileNotFoundError:
+    print("QOS not found. Please check QOS directory.")
+    sys.exit(1)
+except ValueError:
+    print("Python version must be 3.10 or higher.")
+    sys.exit(1)
+
+pfs = platform.system().lower()
+
+try:
+    if pfs == 'windows':
+        if platform.release() < '10':
+            raise ValueError
+except ValueError:
+    print("Error: You must be using Windows 10 or higher to run QOS.")
+    sys.exit(1)
+
+if pfs == 'windows':
+    try:
+        if not os.getenv('PSModulePath'):
+            raise ValueError
+    except ValueError:
+        print("Error: You must be using PowerShell to run QOS.")
+        sys.exit(1)
+
+def activate_venv():
+    print("Making virtual environment...")
+    os.system('python -m venv qosvenv')
+    print("Activating requirements...")
+    if pfs == 'windows':
+        os.system('.\\qosvenv\\Scripts\\activate.ps1')
+    else:
+        os.system('source qosvenv/bin/activate')
+
+def ins_req():
+    print("Installing requirements...")
+    os.system('pip install -r requirements.txt')
+    print("QOS is ready to use.")
+
 def make_path():
     old_path = os.getcwd()
-    new_path = os.path.join(old_path, 'QOS', 'system')
+    new_path = os.path.join(old_path, "QOS")
     os.chdir(new_path)
 
-def check_platform():
-    global f_os
-    if platform.system() == 'Windows':
-        os.system('cls')
-        f_os = 'Windows'
-    elif platform.system() == 'Linux':
-        os.system('clear')
-        f_os = 'Linux'
-    elif platform.system() == 'Darwin':
-        os.system('clear')
-        f_os = 'MacOS'
+def run_qos():
+    if pfs == 'windows':
+        os.system('python qos.py')
     else:
-        f_os = 'Unknown'
+        os.system('python3 qos.py')
 
 def main():
-    if f_os == 'Windows':
-        os.system('python qos.py')
-    elif f_os == 'Linux':
-        os.system('python3 qos.py')
-    elif f_os == 'MacOS':
-        os.system('python3 qos.py')
-    else:
-        print('Unknown platform')
-
-if __name__ == '__main__':
-    check_platform()
+    activate_venv()
+    ins_req()
     make_path()
     try:
-        main()
+        run_qos()
     except KeyboardInterrupt:
-        sys.exit(0)
+        pass
+
+if __name__ == '__main__':
+    main()
